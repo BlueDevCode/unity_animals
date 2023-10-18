dc06bc79b1188e21ad65a6507d823c3e2b646d99
claudiacadec <claudiacadec@outlook.com>
2023-10-18 20:57:56 +0200

    commit

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//@claudiacadec
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
+    private float topBound = 30f;
+    private float lowerBound = -10f;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if((this.transform.position.z > topBound) || 
+          (this.transform.position.z  < lowerBound) )
+        {
+            Destroy(this.gameObject);
+        }
     }
+
 }
