c9e5d5e490a2369d069973b86dcc65d1df854c54
claudiacadec <claudiacadec@outlook.com>
2023-10-18 21:37:47 +0200

    commit

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public int animalIndex;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animalIndex >=0 && animalIndex < enemies.Length){
+            if (Input.GetKeyDown(KeyCode.S))
+            Instantiate( enemies [animalIndex],
+            new Vector3(0,0,transform.position.z),
+            enemies[animalIndex].transform.rotation);
+        }
+        
+    }
+}
